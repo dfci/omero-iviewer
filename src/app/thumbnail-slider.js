5fdbcb6b7e8dae1bac78ffe3cb630723a1f474f4
esiegel <esiegel@jimmy.harvard.edu>
2018-08-02 13:02:07 -0400

    remove hardcoded parent link

--- src/app/thumbnail-slider.js
+++ src/app/thumbnail-slider.js
@@ -429,7 +429,7 @@ export default class ThumbnailSlider extends EventSubscriber {
             (this.context.initial_type === INITIAL_TYPES.IMAGES &&
             parent_type === INITIAL_TYPES.DATASET)) {
                 url += this.web_api_base + DATASETS_REQUEST_URL +
-                    '/' + 4 + '/images/?';
+                    '/' + parent_id + '/images/?';
         } else if (this.context.initial_type === INITIAL_TYPES.WELL ||
                     (this.context.initial_type === INITIAL_TYPES.IMAGES &&
                     parent_type === INITIAL_TYPES.WELL)) {
